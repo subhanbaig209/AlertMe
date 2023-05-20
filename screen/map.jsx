e932b967e6ffebbd43277f0a11d70b520d544a18
Subhan <subhanbaig209@gmail.com>
2023-05-20 15:47:29 -0700

    location

--- screen/map.jsx
+++ screen/map.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function Map() {
+  const [currentLocation, setCurrentLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied.');
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync({});
+      setCurrentLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+
+      Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.BestForNavigation,
+          timeInterval: 5000,
+          distanceInterval: 10,
+        },
+        (newLocation) => {
+          setCurrentLocation({
+            latitude: newLocation.coords.latitude,
+            longitude: newLocation.coords.longitude,
+          });
+        }
+      );
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      {currentLocation && (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+          followsUserLocation={true}
+        />
+      )}
     </View>
   );
 }
