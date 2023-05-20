c9ce7c73a8ea56636f60c7f9c7bcdb7516577596
John Missal <johnmissal@Johns-MacBook-Pro-7.local>
2023-05-20 13:16:42 -0700

    lol

--- App.js
+++ App.js
@@ -1,20 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Button, FlatList, SafeAreaView } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [incidentList, setIncidentList] = useState([]);
+  const [username, setUsername] = useState('');
+  const [description, setDescription] = useState('');
+  const [location, setLocation] = useState('');
+
+  const handleAddIncident = () => {
+    if (username && description && location) {
+      const newIncident = {
+        id: Math.random().toString(),
+        username: username,
+        description: description,
+        location: location,
+      };
+
+      setIncidentList((prevList) => [...prevList, newIncident]);
+      setUsername('');
+      setDescription('');
+      setLocation('');
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Description"
+          value={description}
+          onChangeText={(text) => setDescription(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Location"
+          value={location}
+          onChangeText={(text) => setLocation(text)}
+        />
+        <Button title="Add Incident" onPress={handleAddIncident} />
+      </View>
+
+      <FlatList
+        data={incidentList}
+        renderItem={({ item }) => (
+          <View style={styles.tweetContainer}>
+            <Text style={styles.username}>{item.username}</Text>
+            <Text>Description: {item.description}</Text>
+            <Text>Location: {item.location}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+  inputContainer: {
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 8,
+    paddingHorizontal: 8,
+  },
+  tweetContainer: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  username: {
+    fontWeight: 'bold',
+    marginBottom: 8,
   },
 });
+
+export default App;
